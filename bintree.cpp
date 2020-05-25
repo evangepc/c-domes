64b580217d824b41f03fa3d2108cff87a559a988
sotos24 <37193349+sotos24@users.noreply.github.com>
2020-05-26 01:58:21 +0300

    ksekinhsa epitelous
    //https://www.cprogramming.com/tutorial/lesson18.html
    
    phra to add kai to search etoima, den einai swsta

--- bintree.cpp
+++ bintree.cpp
@@ -1,1 +1,68 @@
 #include "bintree.h"
+
+
+btree::btree()
+{
+  root=NULL;
+}
+
+void btree::insert(int key)   //non recursive insert(also creating the root)
+{
+  if(root!=NULL)
+    insert(key, root);
+  else
+  {
+    root=new node;
+    root->key_value=key;
+    root->left=NULL;
+    root->right=NULL;
+  }
+}
+
+void btree::insert(int key, node *leaf)   //recursive insert
+{
+  if(key< leaf->key_value)
+  {
+    if(leaf->left!=NULL)
+     insert(key, leaf->left);
+    else
+    {
+      leaf->left=new node;
+      leaf->left->key_value=key;
+      leaf->left->left=NULL;    //Sets the left child of the child node to null
+      leaf->left->right=NULL;   //Sets the right child of the child node to null
+    }
+  }
+  else if(key>=leaf->key_value)
+  {
+    if(leaf->right!=NULL)
+      insert(key, leaf->right);
+    else
+    {
+      leaf->right=new node;
+      leaf->right->key_value=key;
+      leaf->right->left=NULL;  //Sets the left child of the child node to null
+      leaf->right->right=NULL; //Sets the right child of the child node to null
+    }
+  }
+}
+
+node *btree::search(int key)   //non recursive search(gives the root)
+{
+  return search(key, root);
+}
+
+node *btree::search(int key, node *leaf)   //recursive search
+{
+  if(leaf!=NULL)
+  {
+    if(key==leaf->key_value)
+      return leaf;
+    if(key<leaf->key_value)
+      return search(key, leaf->left);
+    else
+      return search(key, leaf->right);
+  }
+  else return NULL;
+}
+
--- bintree.h
+++ bintree.h
@@ -1,8 +1,22 @@
+//https://www.cprogramming.com/tutorial/lesson18.html
 
 class bintree{
 
 	public:
+	    bintree();
+        void insert(int key);
+        node *search(int key);
+        void delete(int key);
+
+
+	    struct node{
+            int key;
+            node *left;
+            node *right;};
 
 	private:
+        void insert(int key, node *leaf);
+        node *search(int key, node *leaf);
+        node *root;
 
 };

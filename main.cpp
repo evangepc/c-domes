26b6c6ed0f5850e742ded882243894927e983428
evangepc <62204822+evangepc@users.noreply.github.com>
2020-06-12 13:56:44 +0300

    diabazei arxeia

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "bintree.h"
 #include "avltree.h"
 
 using namespace std;
 
-main(){
-
+main()
+{
+	ifstream file("inputfile.txt");
+	string linestr;
+	while (std::getline(file, linestr))
+	{
+        	// Process linestr (e.g., eliminate punctuation marks, isolate words)
+	}
 	return 0;
 }
